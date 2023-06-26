0cf7243a17076030d518d577a8675c70ab563b64
Débora Moura <118219650+Debyy-moura@users.noreply.github.com>
2023-06-26 19:05:34 -0300

    Att2.26-06-Tquinta

---
+++ att.js
@@ -0,0 +1,25 @@
+// var ex1
+var n1 = 10;
+var n2 = 4;
+var resultado = n1*n2;
+console.log(resultado)
+
+// var ex2
+var sobrenome = Moura;
+console.log(sobrenome)
+
+// let ex1
+let idade = 20;
+console.log(idade)
+
+// let ex2
+let nome = "Débora";
+console.log(nome)
+
+// const ex1
+const valor = 8890928;
+console.log(valor)
+
+// const ex2
+const cpf = 13810446467;
+console.log(cpf)
