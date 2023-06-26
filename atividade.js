9549e08cf55502188639b4edf50a40dd3279c30b
Débora Moura <118219650+Debyy-moura@users.noreply.github.com>
2023-06-26 18:30:28 -0300

    aticidade.26-06 Tquinta

---
+++ atividade.js
@@ -0,0 +1,91 @@
+//treino Js : Number ex1
+var numero = 4;
+console.log(numero)
+
+//treino Js : ex2
+var = 4;
+var n1 = 3;
+var soma = 4 + 3;
+console.log(soma)
+
+// treino Js :String ex1 
+var frase = ('Deixa a vida me leva');
+console.log(frase)
+
+// treino Js :String ex2
+
+var texto ('não sei oq escrever');
+console.log(texto)
+
+// treino Js : Boolean ex1 
+
+var nao = true;
+var sim = false;
+
+console.log("você vai ficar em casa");
+console.log(nao)
+
+console.log("Você vai vim hoje?");
+console.log(sim)
+
+// treino Js : Boolean ex2
+
+var sim = true;
+var nao= false;
+
+console.log("Voce esta trabalhando?");
+console.log(sim)
+
+console.log("Gosta do trabalho");
+console.log(nao)
+
+// treino Js : Array ex1
+var Raccachorro = ["Husky Siberiano", "Goldem","Pastor_alemao", "Labrador"];
+console.log(Raccachorro)
+
+// treino Js : Array ex2
+
+var Racgato= ["Siamês","Maine","Sphynx"];
+console.log(Racgato)
+
+// treino Js : Object ex1
+
+var caneta = {cor: "azul", modelo: "bic"};
+
+console.log(caneta);
+
+console.log(caneta.cor);
+
+console.log(caneta.modelo)
+
+// treino Js : Object ex2
+
+var panela ={ cor: "branca", tamanho: "medio", tampa: "branca"}
+
+console.log(panela);
+
+console.log(panela.cor);
+
+console.log(panela.modelo);
+
+console.log(panela.tampa)
+
+// treino Js :  Undefined e Null ex1
+
+var preco_nao_definido;
+
+console.log("preco_nao_definido: " + preco_nao_definido);
+
+var preco_vazio = null
+
+console.log("preco_vazio: " + preco_vazio);
+
+// treino Js :  Undefined e Null ex2
+
+var numero_nao_definido;
+
+console.log("numero_nao_definido: " + numero_nao_definido);
+
+var numero_vazio = null;
+
+console.log("numero_vazio: " + numero_vazio)
